6c250625380d89a0cdcf67aa40be8f789a1e4361
Jeremy Venezia <veneziajeremy@gmail.com>
2014-08-27 22:13:49 +0200

    more slideshow attributes

--- lib/slideshare_api/model/slideshow.rb
+++ lib/slideshare_api/model/slideshow.rb
@@ -1,16 +1,29 @@
 module SlideshareApi
   module Model
     class Slideshow
-      attr_accessor :id, :title, :description, :username, :url, :embed, :language
+      attr_accessor :id, :title, :description, :status, :username,
+                    :url, :thumbnail_url, :thumbnail_size, :thumbnail_small_url, :embed, :language,
+                    :created_at, :updated_at,
+                    :format, :download, :slideshow_type, :in_contest
 
       def initialize(slideshow_xml)
         @id = slideshow_xml.search('ID').text
         @title = slideshow_xml.search('Title').text
         @description = slideshow_xml.search('Description').text
+        @status = slideshow_xml.search('Status').text
         @username = slideshow_xml.search('Username').text
         @url = slideshow_xml.search('URL').text
+        @thumbnail_url = slideshow_xml.search('ThumbnailURL').text
+        @thumbnail_size = slideshow_xml.search('ThumbnailSize').text
+        @thumbnail_small_url = slideshow_xml.search('ThumbnailSmallURL').text
         @embed = slideshow_xml.search('Embed').text
+        @created_at = Time.parse slideshow_xml.search('Created').text
+        @updated_at = Time.parse slideshow_xml.search('Updated').text
         @language = slideshow_xml.search('Language').text
+        @format = slideshow_xml.search('Format').text
+        @download = slideshow_xml.search('Download').text
+        @slideshow_type = slideshow_xml.search('SlideshowType').text
+        @in_contest = slideshow_xml.search('InContest').text
       end
     end
   end
--- spec/model/slideshow_spec.rb
+++ spec/model/slideshow_spec.rb
@@ -9,9 +9,19 @@ describe SlideshareApi::Model::Slideshow do
     it { expect(subject.id).to eq slideshow_xml.search('ID').text }
     it { expect(subject.title).to eq slideshow_xml.search('Title').text }
     it { expect(subject.description).to eq slideshow_xml.search('Description').text }
+    it { expect(subject.status).to eq slideshow_xml.search('Status').text }
     it { expect(subject.username).to eq slideshow_xml.search('Username').text }
     it { expect(subject.url).to eq slideshow_xml.search('URL').text }
+    it { expect(subject.thumbnail_url).to eq slideshow_xml.search('ThumbnailURL').text }
+    it { expect(subject.thumbnail_size).to eq slideshow_xml.search('ThumbnailSize').text }
+    it { expect(subject.thumbnail_small_url).to eq slideshow_xml.search('ThumbnailSmallURL').text }
     it { expect(subject.embed).to eq slideshow_xml.search('Embed').text }
+    it { expect(subject.created_at).to eq Time.parse slideshow_xml.search('Created').text }
+    it { expect(subject.updated_at).to eq Time.parse slideshow_xml.search('Updated').text }
     it { expect(subject.language).to eq slideshow_xml.search('Language').text }
+    it { expect(subject.format).to eq slideshow_xml.search('Format').text }
+    it { expect(subject.download).to eq slideshow_xml.search('Download').text }
+    it { expect(subject.slideshow_type).to eq slideshow_xml.search('SlideshowType').text }
+    it { expect(subject.in_contest).to eq slideshow_xml.search('InContest').text }
   end
 end
