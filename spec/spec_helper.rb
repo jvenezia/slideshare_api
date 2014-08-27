6b413437d676a410cf51e39a5d5b341c8fb36df9
Jeremy Venezia <veneziajeremy@gmail.com>
2014-08-28 00:17:44 +0200

    add colors in rspec output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,9 @@ Bundler.setup
 require 'slideshare_api'
 
 RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
 end
